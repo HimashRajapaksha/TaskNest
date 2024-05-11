74f4ceeb22e587e1fdeb329f87738253089caf9e
Himash Bhanuka Rajapaksha <rajapakshahimash@gmail.com>
2024-05-12 00:31:39 +0530

    Add And Setup Task Adapter

--- app/src/main/java/com/example/mad_exam_04/MainActivity.kt
+++ app/src/main/java/com/example/mad_exam_04/MainActivity.kt
@@ -3,20 +3,37 @@ package com.example.mad_exam_04
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mad_exam_04.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db: TasksDatabaseHelper
+    private lateinit var tasksAdapter: TasksAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        db =TasksDatabaseHelper(this)
+        tasksAdapter = TasksAdapter(db.getAllTasks(),this)
+
+        binding.tasksRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.tasksRecyclerView.adapter = tasksAdapter
+
+
         binding.addButton.setOnClickListener{
             val intent = Intent(this, AddNewTaskActivity::class.java) // Corrected Intent constructor
             startActivity(intent)
         }
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        tasksAdapter.refreshData(db.getAllTasks())
+    }
+
 }
