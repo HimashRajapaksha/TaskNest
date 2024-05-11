4109b3530cf83f42c2a263152db458d665edcfc1
Himash Bhanuka Rajapaksha <rajapakshahimash@gmail.com>
2024-05-11 20:08:47 +0530

    Add MainActivity

--- app/src/main/java/com/example/mad_exam_04/MainActivity.kt
+++ app/src/main/java/com/example/mad_exam_04/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.mad_exam_04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import com.example.mad_exam_04.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener{
+            val intent = Intent(this, AddNewTaskActivity::class.java) // Corrected Intent constructor
+            startActivity(intent)
+        }
     }
 }
