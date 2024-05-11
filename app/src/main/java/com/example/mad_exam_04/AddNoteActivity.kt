b02b3fe8c4b01a3c562fa9094f0b3703b18825fe
Himash Bhanuka Rajapaksha <rajapakshahimash@gmail.com>
2024-05-11 19:05:07 +0530

    Add Drawable

---
+++ app/src/main/java/com/example/mad_exam_04/AddNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_exam_04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
