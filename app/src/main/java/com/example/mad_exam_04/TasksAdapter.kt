9a93d3ae83b59060cca593c54331240e42557cd4
Himash Bhanuka Rajapaksha <rajapakshahimash@gmail.com>
2024-05-12 12:40:02 +0530

    Implement Task Delete Function

--- app/src/main/java/com/example/mad_exam_04/TasksAdapter.kt
+++ app/src/main/java/com/example/mad_exam_04/TasksAdapter.kt
@@ -7,40 +7,54 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class TasksAdapter(private var tasks:List<Task>,context: Context) :
-    RecyclerView.Adapter<TasksAdapter.TaskViewHolder>(){
+    class TasksAdapter(private var tasks:List<Task>,context: Context) :
+        RecyclerView.Adapter<TasksAdapter.TaskViewHolder>(){
 
-    class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val titleTextView:TextView = itemView.findViewById(R.id.titleTextView)
-        val contentTextView:TextView = itemView.findViewById(R.id.contentTextView)
-        val updateButton:ImageView = itemView.findViewById(R.id.updateButton)
+        private val db :TasksDatabaseHelper = TasksDatabaseHelper(context)
 
-    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.tasks_item,parent,false)
-        return TaskViewHolder(view)
-    }
 
-    override fun getItemCount(): Int =tasks.size
 
-    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        val task = tasks[position]
-        holder.titleTextView.text = task.title
-        holder.contentTextView.text = task.content
+            class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+                val titleTextView:TextView = itemView.findViewById(R.id.titleTextView)
+                val contentTextView:TextView = itemView.findViewById(R.id.contentTextView)
+                val updateButton:ImageView = itemView.findViewById(R.id.updateButton)
+                val deleteButton:ImageView = itemView.findViewById(R.id.deleteButton)
 
-        holder.updateButton.setOnClickListener {
-            val intent = Intent(holder.itemView.context, UpdateTaskActivity::class.java).apply{
-                putExtra("task_id",task.id)
             }
-            holder.itemView.context.startActivity(intent)
-        }
-    }
-    fun refreshData(newTasks : List<Task>){
-        tasks = newTasks
-        notifyDataSetChanged()
-    }
 
-}
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.tasks_item,parent,false)
+                return TaskViewHolder(view)
+            }
+
+            override fun getItemCount(): Int =tasks.size
+
+            override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+                val task = tasks[position]
+                holder.titleTextView.text = task.title
+                holder.contentTextView.text = task.content
+
+                holder.updateButton.setOnClickListener {
+                    val intent = Intent(holder.itemView.context, UpdateTaskActivity::class.java).apply{
+                        putExtra("task_id",task.id)
+                    }
+                    holder.itemView.context.startActivity(intent)
+                }
+
+                holder.deleteButton.setOnClickListener{
+                    db.deleteTask(task.id)
+                    refreshData(db.getAllTasks())
+                    Toast.makeText(holder.itemView.context,"Task Deleted",Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            fun refreshData(newTasks : List<Task>){
+                tasks = newTasks
+                notifyDataSetChanged()
+            }
+
+    }
--- app/src/main/java/com/example/mad_exam_04/TasksDatabaseHelper.kt
+++ app/src/main/java/com/example/mad_exam_04/TasksDatabaseHelper.kt
@@ -82,5 +82,12 @@ class TasksDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE
         return Task(id,title,content)
 
     }
+    fun deleteTask(taskId: Int){
+        val db = writableDatabase
+        val whereClause = "$COLUMN_ID = ?"
+        val whereArgs = arrayOf(taskId.toString())
+        db.delete(TABLE_NAME,whereClause,whereArgs)
+        db.close()
+    }
 
 }
